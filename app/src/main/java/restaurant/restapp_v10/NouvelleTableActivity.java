9a1b34e8a96c444a54711c70b11f669981ab1638
JP2-Warrak <jp2.clecheta@gmail.com>
2017-05-25 14:16:28 +0200

    PushMessage

---
+++ app/src/main/java/restaurant/restapp_v10/NouvelleTableActivity.java
@@ -0,0 +1,8 @@
+package restaurant.restapp_v10;
+
+/**
+ * Created by Alexis on 21/05/2017.
+ */
+
+public class NouvelleTableActivity {
+}
---
+++ app/src/main/java/restaurant/restapp_v10/ParametresActivity.java
@@ -0,0 +1,8 @@
+package restaurant.restapp_v10;
+
+/**
+ * Created by Alexis on 23/05/2017.
+ */
+
+public class ParametresActivity {
+}
